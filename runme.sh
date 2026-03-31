5b599a968f41b8fa36af04826210c8bf3027f4c7
Andrew Yourtchenko <ayourtch@gmail.com>
2026-04-01 00:29:09 +0200

    add other components
    Change-Id: Ie242826c47c7c3e92ddf92944748ba1ba2287e84

--- runme.sh
+++ runme.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 set -eux
 
-git clone https://github.com/ayourtch/oside || true
-git clone https://github.com/ayourtch-llm/ayurl || true
-git clone https://github.com/ayourtch-llm/aycallhome || true
-git clone https://github.com/ayourtch-llm/ayssh || true
-git clone https://github.com/ayourtch-llm/aytelnet || true
-git clone https://github.com/ayourtch-llm/ayclic || true
-git clone https://github.com/ayourtch-llm/aysnmp || true
-cd aysnmp && cargo build
+git clone https://github.com/ayourtch/oside || (cd oside; git pull)
+git clone https://github.com/ayourtch-llm/ayurl || (cd ayurl; git pull)
+git clone https://github.com/ayourtch-llm/aycallhome || (cd aycallhome; git pull)
+git clone https://github.com/ayourtch-llm/ayssh || (cd ayssh; git pull)
+git clone https://github.com/ayourtch-llm/aytelnet || (cd aytelnet; git pull)
+git clone https://github.com/ayourtch-llm/ayclic || (cd ayclic; git pull)
+git clone https://github.com/ayourtch-llm/aysnmp || (cd aysnmp; git pull)
+git clone https://github.com/ayourtch-llm/aycfggen || (cd aycfggen; git pull)
+git clone https://github.com/ayourtch/rust-mustache || (cd rust-mustache ; git pull)
+git clone https://github.com/ayourtch/aycalc || (cd aycalc ; git pull)
+git clone https://github.com/ayourtch-llm/ayiosupdate || (cd ayiosupdate ; git pull)
+(cd aysnmp && cargo build)
+(cd aycfggen && cargo build)
+(cd ayiosupdate && cargo build)
 
 echo "cd aysnmp and have a look at README.md"
 
